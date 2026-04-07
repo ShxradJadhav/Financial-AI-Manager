2de4ba2e96f783334fe7693fb922a461c2efc695
Sharad Jadhav <sharadjadhav1999@gmail.com>
2026-04-07 22:00:28 +0530

    Update database.py

--- database.py
+++ database.py
@@ -3,15 +3,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# 1. Get the URL from Render's environment variable
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-# 2. If no Cloud URL, use your local MySQL
 if not DATABASE_URL:
-    # Change 'root:root' to your local password if different
+    
     DATABASE_URL = "mysql+pymysql://root:root@localhost:3306/finance_db"
 
-# 3. SQLAlchemy fix: Render gives 'postgres://', but SQLAlchemy needs 'postgresql://'
 if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
     DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
