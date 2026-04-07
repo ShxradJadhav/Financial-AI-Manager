7918504d95a572d782b18bf51fde04a0fd5e9465
Sharad Jadhav <sharadjadhav1999@gmail.com>
2026-04-07 21:58:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 from fastapi import FastAPI, Depends
 from sqlalchemy.orm import Session
 import models
-import rag_engine  # <--- Make sure the # is gone!
+import rag_engine  
 from database import engine, get_db
 
-# 1. This creates your MySQL tables
 models.Base.metadata.create_all(bind=engine)
 
-# 2. Start the App
+
 app = FastAPI(title="Financial Doc AI")
 
 @app.get("/")
@@ -27,15 +26,15 @@ async def upload_doc(title: str, company: str, content: str, db: Session = Depen
     db.commit()
     db.refresh(new_doc)
     
-    # Save to Qdrant (The AI Meaning) <--- ADD THIS LINE
+
     rag_engine.index_document(content, new_doc.id, title)
     
     return {"status": "Saved to MySQL and AI Search!", "id": new_doc.id}
 
 @app.get("/rag/search")
 def ai_search(query: str):
-    # This calls the "Search" function from your rag_engine.py file
+    
     results = rag_engine.search_docs(query)
     
-    # We return the parts of the document the AI found most relevant
+    
     return [{"content": res.page_content, "metadata": res.metadata} for res in results]
